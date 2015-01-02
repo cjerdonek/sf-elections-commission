ceee49fbaf337512f106d17d0179be066a08bd50
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-01-02 12:35:37 -0800

    Update run_script.

--- scripts/run_script.py
+++ scripts/run_script.py
@@ -1,8 +1,13 @@
 
+from __future__ import absolute_import
+
 import logging
 
 from electcomm import email, tweet
+from electcomm.formatter import Formatter
+
 
 if __name__ == '__main__':
     logging.basicConfig(level='INFO')
-    email.main()
+    f = Formatter('2015_01_07_bopec')
+    print(f.make_html_index_announce())
