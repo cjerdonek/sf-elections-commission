8bb342c0a5a2e3df8b6790a1d8e8551d168864c2
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-01-02 07:41:27 -0800

    Work more on tweeting.

--- scripts/tweet.py
+++ scripts/tweet.py
@@ -1,11 +1,51 @@
 
+from datetime import date
 import os
+import re
 import shlex
 
 import tweepy
 import yaml
 
 USERNAME = "SFElectionsComm"
+NAME_BOPEC = "Budget & Oversight of Public Elections Committee (BOPEC)"
+NAME_COMMISSION = "Elections Commission"
+WEB_SITE_HOME = "http://www.sfgov2.org/index.aspx?page=319"
+WEB_SITE_MEETINGS = "http://www.sfgov2.org/index.aspx?page=1382"
+
+BODY_NAMES = {'bopec': NAME_BOPEC, 'commission': NAME_COMMISSION}
+URL_PATTERN = re.compile(r"http://\S*")
+
+
+# All URLs get replaced by 22-character URLs (even if the original is shorter):
+# https://support.twitter.com/articles/78124-posting-links-in-a-tweet
+def get_length(tweet):
+    """Calculate how long the URL will be once Twitter shortens it."""
+    new_string, url_count = URL_PATTERN.subn("", tweet)
+    return len(new_string) + url_count * 22
+
+
+def parse_label(label):
+    """For example: 2015_01_07_bopec."""
+    parts = label.split("_")
+    body = parts.pop()
+    body_name = BODY_NAMES[body]
+    year, month, day = (int(s) for s in parts)
+    dt = date(year, month, day)
+    return body_name, dt
+
+
+def make_tweet(format_string, label):
+    body, date = parse_label(label)
+    return format_string.format(date=date, day=date.day, body=body,
+                                home_page=WEB_SITE_HOME)
+
+def get_cancel_tweet(label):
+    # Date for example: Wed, January 7, 2015
+    format = ("The {0} meeting of the {{body}} will not be held: {{home_page}}"
+              .format("{date:%a, %B {day}, %Y}"))
+    return make_tweet(format, label)
+
 
 def get_config():
     repo_root = os.path.join(os.path.dirname(__file__), os.pardir)
@@ -68,15 +108,22 @@ def tweet(message=None):
         message = input("message: ").strip()
     auth = get_oauth(username)
     api = tweepy.API(auth)
-    response = input('Are you sure you want to tweet this through '
-                     'user: {user}?\n "{text}" [{length} chars]\n(yes/no)? '
-                     .format(user=username, length=len(message), text=message))
+    response = input('Are you sure you want to tweet this?\n'
+                     'user: {user}\n'
+                     'text: "{text}"\n'
+                     'length: {length} chars (reduced from {old_length})\n'
+                     '(yes/no)? '
+                     .format(user=username, text=message, length=get_length(message),
+                             old_length=len(message)))
     if response != 'yes':
         exit('** Aborted.')
     api.update_status(message)
 
 
 def main():
+    label = '2015_01_07_bopec'
+    print(get_cancel_tweet(label))
+    exit()
     tweet()
 
 
