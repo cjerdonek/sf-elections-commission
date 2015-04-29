1beb67932a05d935c70ecf6de8f59443905cfc91
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-04-29 14:10:40 -0700

    Cancel meeting.

--- pycomm/config.py
+++ pycomm/config.py
@@ -11,21 +11,29 @@ from pycomm import common
 REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 
-def get_config_file_path(file_name):
-    config_dir = os.path.join(REPO_DIR, 'config')
+def read_yaml(path):
+    with open(path) as f:
+        data = yaml.load(f)
+    return data
+
+
+def get_config_file_path(file_name, dir_path=None):
+    if dir_path is None:
+        config_dir = os.path.join(REPO_DIR, 'config')
+    else:
+        config_dir = dir_path
     return os.path.join(config_dir, file_name)
 
 
-def get_config_file(file_name):
-    config_path = get_config_file_path(file_name)
-    with open(config_path) as f:
-        data = yaml.load(f)
+def get_config_file(file_name, dir_path=None):
+    config_path = get_config_file_path(file_name, dir_path=dir_path)
+    data = read_yaml(config_path)
     return data
 
 
 def get_config():
     people_data = get_config_file('people.secret.yaml')
-    meeting_data = get_config_file('meetings.yaml')['meetings']
+    meeting_data = get_config_file('meetings.yaml', dir_path=REPO_DIR)['meetings']
 
     config = Config()
     config.meetings = meeting_data
